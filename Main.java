d5a02285cdb5bb7953d60e9331fcf8a2b98d7d34
Guminc1 <zombiekillerranger@gmail.com>
2025-10-02 17:40:17 +0000

    Finish

--- Main.java
+++ Main.java
@@ -2,30 +2,45 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write your code here
+    lastFirstN("cream", "butter", 4);
+    stringManip("apple", "banana");
+    removeStr("bdaboom", "ada");
   }
 
-  /** Precondition: s1 and s2 are not null
-  * Precondition: 0 <= n <= min(s1.length(), s2.length()
-  */
   public static void lastFirstN(String s1, String s2, int n)
   {
-  
+    String firstN;
+    String lastN;
+    String output;
+    
+    int s1Len = s1.length();
+    int s2Len = s2.length();
+
+    firstN = s1.substring(s1Len-n,s1Len);
+    lastN = s2.substring(0,n);
+
+    output = firstN + lastN;
+    System.out.println(output);
   }
 
-  /** Precondition: s1 and s2 are not null
-  */
   public static void stringManip(String s1, String s2)
   {
-  
+    s1 = s1.toUpperCase();
+    s2 = s2.toUpperCase();
+    String s3;
+    s3 = s2.substring(1,s2.length());
+    s3 = s3.toLowerCase();
+    s2 = s2.substring(0,1);
+    s2 = s2 + s3;
+    System.out.println(s1 + " " + s2);
   }
 
-  /** Precondition: s1 and s2 are not null
-  * Precondition: s2 is a substring of s1 and can be found at least one time in s1
-  */
-  public static String removeStr(String s1, String s2)
+  public static void removeStr(String s1, String s2)
   {
     String output = null;
-    return output;
+    int indexS = s1.indexOf(s2);
+    int indexE = indexS + s2.length();
+    output = s1.substring(0,indexS) + s1.substring(indexE,s1.length());
+    System.out.println(output);
   }
 }
